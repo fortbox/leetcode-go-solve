17a3179c90be74625280ac587803f41dd39ad184
henryxiao <fortbox1988@hotmail.com>
2020-10-05 21:04:02 +0800

    560 2020-10-05

---
+++ 560.go
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2020
+ * @Author: xiaoweixiang
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 1, 1}
+	k := 2
+	ans := subarraySum(nums, k)
+	fmt.Println("ans:", ans)
+
+}
+func subarraySum(nums []int, k int) int {
+	ma := make(map[int]int)
+	sum := 0
+	res := 0
+	ma[0] = 1
+	for _, num := range nums {
+		sum += num
+		v, okk := ma[sum]
+		if okk {
+			ma[sum] = v + 1
+		} else {
+			ma[sum] = 1
+		}
+		value, ok := ma[sum-k]
+		if ok {
+			res += value
+		}
+		fmt.Println("ma:", ma)
+	}
+	return res
+}
